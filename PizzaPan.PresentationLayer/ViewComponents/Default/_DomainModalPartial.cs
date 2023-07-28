53197e6966e5bc6b01dcd410d1a91924d6cc106f
Samet DOGAN <sametdogan23@protonmail.ch>
2023-07-28 11:06:46 +0300

    footer and domain part completed

--- PizzaPan.PresentationLayer/ViewComponents/Default/_DomainModalPartial.cs
+++ PizzaPan.PresentationLayer/ViewComponents/Default/_DomainModalPartial.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzaPan.BusinessLayer.Abstract;
+using PizzaPan.EntityLayer.Concrete;
+using System.Linq;
 
 namespace PizzaPan.PresentationLayer.ViewComponents.Default
 {
+    [AllowAnonymous]
     public class _DomainModalPartial : ViewComponent
     {
+        private readonly ICompanyInfoService _companyInfoService;
+
+        public _DomainModalPartial(ICompanyInfoService companyInfoService)
+        {
+            _companyInfoService = companyInfoService;
+        }
+
         public IViewComponentResult Invoke()
         {
+            CompanyInfo companyInfos = new CompanyInfo();
+            companyInfos = _companyInfoService.TGetList().FirstOrDefault();
+            ViewBag.CompanyInfo = companyInfos;
+
             return View();
         }
     }
--- PizzaPan.PresentationLayer/ViewComponents/Default/_FooterPartial.cs
+++ PizzaPan.PresentationLayer/ViewComponents/Default/_FooterPartial.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.BusinessLayer.Abstract;
+using PizzaPan.EntityLayer.Concrete;
+using System.Linq;
 
 namespace PizzaPan.PresentationLayer.ViewComponents.Default
 {
     public class _FooterPartial:ViewComponent
     {
+        private readonly ICompanyInfoService _companyInfoService;
+
+        public _FooterPartial(ICompanyInfoService companyInfoService)
+        {
+            _companyInfoService = companyInfoService;
+        }
+
         public IViewComponentResult Invoke()
         {
+            CompanyInfo companyInfos = new CompanyInfo();
+            companyInfos = _companyInfoService.TGetList().FirstOrDefault();
+            ViewBag.CompanyInfo = companyInfos;
+
             return View();
         }
     }
