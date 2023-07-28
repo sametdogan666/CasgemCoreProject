77949999d8c24c496a9f470ef9acc76031636a84
Samet DOGAN <sametdogan23@protonmail.ch>
2023-07-28 10:51:00 +0300

    send message and companyInfos added

--- PizzaPan.PresentationLayer/Controllers/ContactController.cs
+++ PizzaPan.PresentationLayer/Controllers/ContactController.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzaPan.BusinessLayer.Abstract;
+using PizzaPan.EntityLayer.Concrete;
 
 namespace PizzaPan.PresentationLayer.Controllers
 {
+    [AllowAnonymous]
     public class ContactController : Controller
     {
-        // GET
+        private readonly ICompanyInfoService _companyInfoService;
+        private readonly IContactService _contactService;
+
+        public ContactController(ICompanyInfoService companyInfoService, IContactService contactService)
+        {
+            _companyInfoService = companyInfoService;
+            _contactService = contactService;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            CompanyInfo companyInfos = new CompanyInfo();
+            companyInfos = _companyInfoService.TGetList().FirstOrDefault();
+            ViewBag.CompanyInfo = companyInfos;
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactSendDateMessage = DateTime.Now;
+            _contactService.TInsert(contact);
+
+            return RedirectToAction("Index", "Default");
         }
     }
 }
--- PizzaPan.PresentationLayer/Controllers/DefaultController.cs
+++ PizzaPan.PresentationLayer/Controllers/DefaultController.cs
@@ -6,7 +6,6 @@ namespace PizzaPan.PresentationLayer.Controllers
     [AllowAnonymous]
     public class DefaultController : Controller
     {
-        
         public IActionResult Index()
         {
             return View();
